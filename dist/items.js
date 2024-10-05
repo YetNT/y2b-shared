105bd78152d9a8ed4da494f8ab6d0a311aeeda03
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-10-05 16:25:11 +0200

    dist

--- dist/items.js
+++ dist/items.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Item = exports.Rarity = void 0;
+/** Item rarities */
 var Rarity;
 (function (Rarity) {
     Rarity["common"] = "Common";
@@ -10,9 +11,34 @@ var Rarity;
     Rarity["insane"] = "Insane";
     Rarity["godly"] = "Godly";
 })(Rarity || (exports.Rarity = Rarity = {}));
-/** An Item */
+/** An Item, i literally have nothing more to say. */
 class Item {
-    constructor(canBeStolen, name, description, uses, price, canBeSold = false, id, rarity, emoji = undefined, useable = false, craftingRecipe = {}) {
+    constructor(
+    /** Indicates whether the item can be stolen. */
+    canBeStolen, 
+    /** The name of the item. */
+    name, 
+    /** A description of the item. */
+    description, 
+    /** An array of descriptions of the item's uses. */
+    uses, 
+    /** The amount of coins required to buy the item. */
+    price, 
+    /** The amount of coins you receive when selling the item.
+     * If set to -1, the item cannot be sold.
+     * Defaults to false.
+     */
+    canBeSold = false, 
+    /** The unique identifier of the item. */
+    id, 
+    /** The rarity of the item using the enum. */
+    rarity, 
+    /** The emoji representation of the item. */
+    emoji = undefined, 
+    /** Indicates whether the item is usable. Defaults to false. */
+    useable = false, 
+    /** The crafting recipe of the item. If undefined, you can't craft it. */
+    craftingRecipe = {}) {
         this.canBeStolen = canBeStolen === true;
         this.craftingRecipe =
             Object.entries(craftingRecipe).length === 0
